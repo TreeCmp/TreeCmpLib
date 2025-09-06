fe78e37f43609b855e82cffa1db88d822a706b1a
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-09-06 18:12:58 +0200

    added tests with null trees

--- src/test/java/treecmp/metrics/RFMetricTest.java
+++ src/test/java/treecmp/metrics/RFMetricTest.java
@@ -7,6 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class RFMetricTest {
 
     @Test
+    void getRFDistance_bothTreeAsNull_returnsException() {
+        NullPointerException exception = assertThrows(
+                NullPointerException.class,
+                () -> RFMetric.getRFDistance(null, null)
+        );
+        assertNull(exception.getMessage());
+    }
+
+    @Test
+    void getRFDistance_firstTreeAsNull_returnsException() {
+        var t1 = TestTreeFactory.simpleTreeA();
+
+        NullPointerException exception = assertThrows(
+                NullPointerException.class,
+                () -> RFMetric.getRFDistance(null, t1)
+        );
+        assertNull(exception.getMessage());
+    }
+
+    @Test
+    void getRFDistance_secondTreeAsNull_returnsException() {
+        var t1 = TestTreeFactory.simpleTreeA();
+
+        NullPointerException exception = assertThrows(
+                NullPointerException.class,
+                () -> RFMetric.getRFDistance(t1, null)
+        );
+        assertNull(exception.getMessage());
+    }
+
+    @Test
     void getRFDistance_identicalTrees_returnsZero() {
         var t1 = TestTreeFactory.simpleTreeA();
         var t2 = TestTreeFactory.simpleTreeA();
