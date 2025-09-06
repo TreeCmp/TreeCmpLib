bdb853cb86700ae34e7fff43e6be7ba4d01dde77
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-09-06 21:43:12 +0200

    added QuartetMetricLong  distance with tests

---
+++ src/test/java/treecmp/metrics/topological/QuartetMetricLongTest.java
@@ -0,0 +1,42 @@
+package treecmp.metrics.topological;
+
+import org.junit.jupiter.api.Test;
+import treecmp.metrics.topological.topological.QuartetMetricDouble;
+import treecmp.metrics.topological.topological.QuartetMetricLong;
+import treecmp.metrics.topological.util.TestTreeFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QuartetMetricLongTest {
+
+    @Test
+    void getQuartetLongDistance_identicalTrees_returnsZero() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree1();
+
+        Double distance = QuartetMetricLong.getQuartetDistance(t1, t2);
+
+        assertEquals(0.0, distance);
+    }
+
+    @Test
+    void getQuartetLongDistance_4leafsTrees_returnsOne() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree2();
+
+        Double distance = QuartetMetricLong.getQuartetDistance(t1, t2);
+
+        assertEquals(1.0, distance);
+    }
+
+    @Test
+    void getQuartetLongDistance_10leafsTrees_returnsOneHundredAndFiftySix() {
+        var t1 = TestTreeFactory.tenLeavesBinaryUnrootedTree1();
+        var t2 = TestTreeFactory.tenLeavesBinaryUnrootedTree2();
+
+        Double distance = QuartetMetricLong.getQuartetDistance(t1, t2);
+
+        assertEquals(156.0, distance);
+    }
+
+}
