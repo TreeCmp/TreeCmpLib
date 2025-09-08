f4c9d053a6297dc28e15fa7fbf8ec2e0eda1a71e
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-09-08 12:09:20 +0200

    added RMAST distance with tests

---
+++ src/test/java/treecmp/metrics/topological/RMASTMetricTest.java
@@ -0,0 +1,45 @@
+package treecmp.metrics.topological;
+
+import org.junit.jupiter.api.Test;
+import treecmp.metrics.topological.topological.NodalL2Metric;
+import treecmp.metrics.topological.util.TestTreeFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RMASTMetricTest {
+
+    @Test
+    void getRMASTDistance_identicalTrees_returnsZero() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree1();
+        var rm = new RMASTMetric();
+
+        double distance = rm.getDistance(t1, t2);
+
+        assertEquals(0.0, distance);
+    }
+
+    @Test
+    void getRMASTDistance_4leafsTrees_returnsFour() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree2();
+
+        var rm = new RMASTMetric();
+
+        double distance = rm.getDistance(t1, t2);
+
+        assertEquals(2.0, distance);
+    }
+
+    @Test
+    void getRMASTDistance_10leafsTrees_returnsEightyFour() {
+        var t1 = TestTreeFactory.tenLeavesBinaryRootedTree1();
+        var t2 = TestTreeFactory.tenLeavesBinaryRootedTree2();
+
+        var rm = new RMASTMetric();
+
+        double distance = rm.getDistance(t1, t2);
+
+        assertEquals(5.0, distance);
+    }
+}
