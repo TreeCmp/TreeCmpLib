1efd311af6b4fa4388fa8c28ae0681b5c352ecbb
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-09-08 11:48:19 +0200

    added NodalL2 distance with tests

---
+++ src/test/java/treecmp/metrics/topological/NodalL2MetricTest.java
@@ -0,0 +1,46 @@
+package treecmp.metrics.topological;
+
+import org.junit.jupiter.api.Test;
+import treecmp.metrics.topological.topological.NodalL2Metric;
+import treecmp.metrics.topological.topological.TripletMetricSimple;
+import treecmp.metrics.topological.util.TestTreeFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NodalL2MetricTest {
+
+    @Test
+    void getNodalL2Distance_identicalTrees_returnsZero() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree1();
+        var nm = new NodalL2Metric();
+
+        double distance = nm.getDistance(t1, t2);
+
+        assertEquals(0.0, distance);
+    }
+
+    @Test
+    void getNodalL2Distance_4leafsTrees_returnsFour() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree2();
+
+        var nm = new NodalL2Metric();
+
+        double distance = nm.getDistance(t1, t2);
+
+        assertEquals(4.0, distance);
+    }
+
+    @Test
+    void getNodalL2Distance_10leafsTrees_returnsEightyFour() {
+        var t1 = TestTreeFactory.tenLeavesBinaryRootedTree1();
+        var t2 = TestTreeFactory.tenLeavesBinaryRootedTree2();
+
+        var nm = new NodalL2Metric();
+
+        double distance = nm.getDistance(t1, t2);
+
+        assertEquals(18.681541692269406, distance, 0.00000000000001);
+    }
+}
