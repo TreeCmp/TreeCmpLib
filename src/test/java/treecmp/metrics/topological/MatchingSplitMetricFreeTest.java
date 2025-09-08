51e2e44675d1863ca2b58fbfde0e233cbf0034b6
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-09-08 14:01:10 +0200

    added MatchingSplitFree distance class and test

---
+++ src/test/java/treecmp/metrics/topological/MatchingSplitMetricFreeTest.java
@@ -0,0 +1,46 @@
+package treecmp.metrics.topological;
+
+import org.junit.jupiter.api.Test;
+import treecmp.metrics.topological.util.TestTreeFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MatchingSplitMetricFreeTest {
+
+    @Test
+    void getMatchingSplitDistance_identicalTrees_returnsZero() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree1();
+
+        var m3m = new MatchingSpliMetricFree();
+
+        double distance = m3m.getDistance(t1, t2);
+
+        assertEquals(0.0, distance);
+    }
+
+    @Test
+    void getMatchingSplitDistance_4leafsTrees_returnsTwo() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree2();
+
+        var m3m = new MatchingSpliMetricFree();
+
+        double distance = m3m.getDistance(t1, t2);
+
+        assertEquals(2.0, distance);
+    }
+
+    @Test
+    void getMatchingSplitDistance_10leafsTrees_returnsSeven() {
+        var t1 = TestTreeFactory.tenLeavesBinaryUnrootedTree1();
+        var t2 = TestTreeFactory.tenLeavesBinaryUnrootedTree2();
+
+        var m3m = new MatchingSpliMetricFree();
+
+        double distance = m3m.getDistance(t1, t2);
+
+        assertEquals(84.0, distance);
+    }
+
+}
