ace879e2d2841b39f03f289fd1767e7baba5a1d7
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-09-08 13:34:44 +0200

    added MatchingCluster distance class and test

---
+++ src/test/java/treecmp/metrics/topological/MatchingClusterMetricTest.java
@@ -0,0 +1,44 @@
+package treecmp.metrics.topological;
+
+import org.junit.jupiter.api.Test;
+import treecmp.metrics.topological.util.TestTreeFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MatchingClusterMetricTest {
+
+    @Test
+    void getMatchingClusterDistance_identicalTrees_returnsZero() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree1();
+        var mcm = new MatchingClusterMetric();
+
+        double distance = mcm.getDistance(t1, t2);
+
+        assertEquals(0.0, distance);
+    }
+
+    @Test
+    void getMatchingClusterDistance_4leafsTrees_returnsFour() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree2();
+
+        var mcm = new MatchingClusterMetric();
+
+        double distance = mcm.getDistance(t1, t2);
+
+        assertEquals(4.0, distance);
+    }
+
+    @Test
+    void getMatchingClusterDistance_10leafsTrees_returnsEightyFour() {
+        var t1 = TestTreeFactory.tenLeavesBinaryRootedTree1();
+        var t2 = TestTreeFactory.tenLeavesBinaryRootedTree2();
+
+        var mcm = new MatchingClusterMetric();
+
+        double distance = mcm.getDistance(t1, t2);
+
+        assertEquals(23.0, distance);
+    }
+}
