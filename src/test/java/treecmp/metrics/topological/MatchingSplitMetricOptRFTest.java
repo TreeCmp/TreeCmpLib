cfdffc281c8fe3ced7cbf75b6db9c2aa8087f9ee
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-09-08 14:46:26 +0200

    added MatchingSplitMetricOptRF distance class and test

---
+++ src/test/java/treecmp/metrics/topological/MatchingSplitMetricOptRFTest.java
@@ -0,0 +1,45 @@
+package treecmp.metrics.topological;
+
+import org.junit.jupiter.api.Test;
+import treecmp.metrics.topological.util.TestTreeFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MatchingSplitMetricOptRFTest {
+
+    @Test
+    void getMatchingSplitDistance_identicalTrees_returnsZero() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+
+        var ms = new MatchingSplitMetricOptRF();
+
+        double distance = ms.getDistance(t1, t1);
+
+        assertEquals(0.0, distance);
+    }
+
+    @Test
+    void getMatchingSplitDistance_4leafsTrees_returnsTwo() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree2();
+
+        var ms = new MatchingSplitMetricOptRF();
+
+        double distance = ms.getDistance(t1, t2);
+
+        assertEquals(4.0, distance);
+    }
+
+    @Test
+    void getMatchingSplitDistance_10leafsTrees_returnsSeven() {
+        var t1 = TestTreeFactory.tenLeavesBinaryUnrootedTree1();
+        var t2 = TestTreeFactory.tenLeavesBinaryUnrootedTree2();
+
+        var ms = new MatchingSplitMetricOptRF();
+
+        double distance = ms.getDistance(t1, t2);
+
+        assertEquals(21, distance);
+    }
+
+}
