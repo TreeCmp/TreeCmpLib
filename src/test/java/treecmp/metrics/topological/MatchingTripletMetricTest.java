57a05cd86fd7b708b9cbeae337ca60e53e7d702e
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-09-08 13:52:20 +0200

    added MatchingPairUnrooted and Triplet distance class and test

---
+++ src/test/java/treecmp/metrics/topological/MatchingTripletMetricTest.java
@@ -0,0 +1,46 @@
+package treecmp.metrics.topological;
+
+import org.junit.jupiter.api.Test;
+import treecmp.metrics.topological.util.TestTreeFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MatchingTripletMetricTest {
+
+    @Test
+    void getMatchingTripletDistance_identicalTrees_returnsZero() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree1();
+
+        var m3m = new MatchingPairMetric();
+
+        double distance = m3m.getDistance(t1, t2);
+
+        assertEquals(0.0, distance);
+    }
+
+    @Test
+    void getMatchingTripletDistance_4leafsTrees_returnsTwo() {
+        var t1 = TestTreeFactory.fourLeavesTree1();
+        var t2 = TestTreeFactory.fourLeavesTree2();
+
+        var m3m = new MatchingPairMetric();
+
+        double distance = m3m.getDistance(t1, t2);
+
+        assertEquals(4.0, distance);
+    }
+
+    @Test
+    void getMatchingTripletDistance_10leafsTrees_returnsSeven() {
+        var t1 = TestTreeFactory.tenLeavesBinaryUnrootedTree1();
+        var t2 = TestTreeFactory.tenLeavesBinaryUnrootedTree2();
+
+        var m3m = new MatchingPairMetric();
+
+        double distance = m3m.getDistance(t1, t2);
+
+        assertEquals(28.0, distance);
+    }
+
+}
