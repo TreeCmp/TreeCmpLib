a97155a5927eb7bf3e590d902b5776e276371b5f
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-10-03 14:38:26 +0200

    added spr tests

---
+++ src/test/java/treecmp/spr/TreeRootedHolderTest.java
@@ -0,0 +1,16 @@
+package treecmp.spr;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class TreeRootedHolderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+}
---
+++ src/test/java/treecmp/spr/UsprHeuristicBaseMetricTest.java
@@ -0,0 +1,15 @@
+package treecmp.spr;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class UsprHeuristicBaseMetricTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
