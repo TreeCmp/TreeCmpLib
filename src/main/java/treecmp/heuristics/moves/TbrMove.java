aeb184e8a5b955b9aa4c54380791dd740607056d
Tomasz Goluch <goluch@eti.pg.edu.pl>
2026-07-12 22:03:23 +0200

    refactor(tbr): implement generic incremental heuristic and fix unrooted neighborhood walker

--- src/main/java/treecmp/heuristics/moves/TbrMove.java
+++ src/main/java/treecmp/heuristics/moves/TbrMove.java
@@ -1,4 +1,37 @@
 package treecmp.heuristics.moves;
 
-public class TbrMove {
+import pal.tree.Node;
+
+/**
+ * Klasa reprezentująca pojedynczy ruch TBR (Tree Bisection and Reconnection).
+ * Przechowuje pełną informację o strukturze ruchu do jego późniejszego fizycznego zaaplikowania.
+ */
+public class TbrMove implements TreeMove {
+
+    public final Node movingNode;  // Odcięty korzeń (pruneNode)
+    public final Node rerootNode;  // Nowy korzeń po przekorzenieniu odciętego poddrzewa
+    public final Node targetNode;  // Docelowe miejsce wpięcia w głównym drzewie
+
+    public TbrMove(Node movingNode, Node rerootNode, Node targetNode) {
+        this.movingNode = movingNode;
+        this.rerootNode = rerootNode;
+        this.targetNode = targetNode;
+    }
+
+    @Override
+    public String getDescription() {
+        return "TbrMove[prune=" + getNodeName(movingNode) +
+                ", reroot=" + getNodeName(rerootNode) +
+                ", target=" + getNodeName(targetNode) + "]";
+    }
+
+    @Override
+    public String toString() {
+        return getDescription();
+    }
+
+    private String getNodeName(Node n) {
+        if (n == null) return "null";
+        return n.isLeaf() ? n.getIdentifier().getName() : "Internal_" + n.getNumber();
+    }
 }
--- src/main/java/treecmp/heuristics/tbr/TbrNeighborhoodWalker.java
+++ src/main/java/treecmp/heuristics/tbr/TbrNeighborhoodWalker.java
@@ -1,149 +1,162 @@
-//package treecmp.heuristics.tbr;
-//
-//import pal.tree.Node;
-//import pal.tree.Tree;
-//import treecmp.heuristics.moves.NniMove;
-//import treecmp.heuristics.moves.VirtualNniMove;
-//import treecmp.metrics.IncrementalMetric;
-//import treecmp.metrics.topological.BaseRFIncrementalMetric;
-//
-//import java.util.ArrayList;
-//import java.util.BitSet;
-//import java.util.List;
-//
-//public class TbrNeighborhoodWalker {
-//
-//    public interface TbrVisitor {
-//        void visit(double distance, Node pruneNode, Node rerootNode, Node targetNode);
-//    }
-//
-//    private TbrUtils tbrUtils = new TbrUtils();
-//
-//    public void walk(Tree baseTree, IncrementalMetric metric, TbrVisitor visitor) {
-//        List<Node> allNodes = getAllNodes(baseTree);
-//
-//        for (Node pruneNode : allNodes) {
-//            if (pruneNode.isRoot() || pruneNode.getParent() == null) continue;
-//
-//            // Wirtualną bramą (Gateway) łączącą poddrzewo z resztą drzewa jest na początku brat pruneNode
-//            Node startGateway = getSibling(pruneNode);
-//            if (startGateway == null) continue;
-//
-//            // Wymiar 2: Rerooting DFS - Rozpoczynamy przekorzenianie odciętej gałęzi
-//            traverseReroot(pruneNode, pruneNode, startGateway, pruneNode.getParent(), metric, visitor);
-//        }
-//    }
-//
-//    private void traverseReroot(Node pruneNode, Node currentReroot, Node mainTreeGateway, Node cameFrom,
-//                                IncrementalMetric metric, TbrVisitor visitor) {
-//
-//        // Wymiar 3: Dla konkretnego ułożenia korzenia, odpalamy spacer po głównym drzewie
-//        traverseRegraft(pruneNode, currentReroot, mainTreeGateway, currentReroot, metric, visitor);
-//
-//        // Zmiana korzenia (Schodzimy NNI w dół odciętego poddrzewa)
-//        if (!currentReroot.isLeaf()) {
-//            for (int i = 0; i < currentReroot.getChildCount(); i++) {
-//                Node child = currentReroot.getChild(i);
-//                if (child == cameFrom || child == mainTreeGateway) continue;
-//
-//                Node otherChild = (i == 0) ? currentReroot.getChild(1) : currentReroot.getChild(0);
-//                if (otherChild != null) {
-//                    // Krok rNNI: Wirtualny korzeń (currentReroot) traci gałąź (otherChild), a zyskuje wejście do głównego drzewa
-//                    NniMove rerootMove = new VirtualNniMove(otherChild, mainTreeGateway, currentReroot);
-//
-//                    metric.applyNni(rerootMove); // Odkładamy na stos
-//
-//                    // Rekurencja z nowym korzeniem
-//                    traverseReroot(pruneNode, child, mainTreeGateway, currentReroot, metric, visitor);
-//
-//                    metric.undoNni(rerootMove); // Ściągamy ze stosu
-//                }
-//            }
-//        }
-//    }
-//
-//    private boolean traverseRegraft(Node pruneNode, Node rerootNode, Node currentTarget, Node cameFrom,
-//                                    IncrementalMetric metric, TbrVisitor visitor) {
-//
-//        // 1. RUCH W GÓRĘ GŁÓWNEGO DRZEWA (rNNI UP)
-//        Node p = currentTarget.getParent();
-//        if (p != null && p != cameFrom && p != pruneNode) {
-//            Node aunt = getSibling(p);
-//            if (aunt != null && aunt != pruneNode) {
-//                // Krok rNNI: Węzeł 'p' wymienia odcięte poddrzewo (rerootNode) na rodzeństwo (aunt)
-//                NniMove upMove = new VirtualNniMove(rerootNode, aunt, p);
-//
-//                metric.applyNni(upMove);
-//
-//                if (tbrUtils.isValidTbrMove(pruneNode, rerootNode, p)) {
-//                    double exactDist = evaluate(metric, pruneNode, rerootNode, p);
-//                    visitor.visit(exactDist, pruneNode, rerootNode, p);
-//                    if (exactDist == 0) { metric.undoNni(upMove); return false; }
-//                }
-//
-//                if (!traverseRegraft(pruneNode, rerootNode, p, currentTarget, metric, visitor)) {
-//                    metric.undoNni(upMove); return false;
-//                }
-//                metric.undoNni(upMove);
-//            }
-//        }
-//
-//        // 2. RUCH W DÓŁ GŁÓWNEGO DRZEWA (rNNI DOWN)
-//        if (!currentTarget.isLeaf()) {
-//            for (int i = 0; i < currentTarget.getChildCount(); i++) {
-//                Node child = currentTarget.getChild(i);
-//                if (child == cameFrom || child == pruneNode) continue;
-//
-//                Node otherChild = (i == 0) ? currentTarget.getChild(1) : currentTarget.getChild(0);
-//                if (otherChild != null) {
-//                    // Krok rNNI: Węzeł 'currentTarget' wymienia 'otherChild' na wędrujące poddrzewo ('rerootNode')
-//                    NniMove downMove = new VirtualNniMove(otherChild, rerootNode, currentTarget);
-//
-//                    metric.applyNni(downMove);
-//
-//                    if (tbrUtils.isValidTbrMove(pruneNode, rerootNode, child)) {
-//                        double exactDist = evaluate(metric, pruneNode, rerootNode, child);
-//                        visitor.visit(exactDist, pruneNode, rerootNode, child);
-//                        if (exactDist == 0) { metric.undoNni(downMove); return false; }
-//                    }
-//
-//                    if (!traverseRegraft(pruneNode, rerootNode, child, currentTarget, metric, visitor)) {
-//                        metric.undoNni(downMove); return false;
-//                    }
-//                    metric.undoNni(downMove);
-//                }
-//            }
-//        }
-//        return true;
-//    }
-//
-//    // Ekstrakcja dokładnego dystansu, jeśli metryka to RF
-//    private double evaluate(IncrementalMetric metric, Node prune, Node reroot, Node target) {
-//        if (metric instanceof BaseRFIncrementalMetric) {
-//            BaseRFIncrementalMetric rfMetric = (BaseRFIncrementalMetric) metric;
-//            BitSet movingBits = rfMetric.getCluster(reroot);
-//            return rfMetric.evaluateExactTbrDistance(prune, reroot, target, movingBits);
-//        }
-//        // Dla innych metryk (np. MP/MC), zwracamy to co jest na stosie (wymaga osobnej obsługi widm)
-//        return metric.getCurrentDistance();
-//    }
-//
-//    private Node getSibling(Node node) {
-//        Node parent = node.getParent();
-//        if (parent == null || parent.getChildCount() < 2) return null;
-//        return (parent.getChild(0) == node) ? parent.getChild(1) : parent.getChild(0);
-//    }
-//
-//    private List<Node> getAllNodes(Tree tree) {
-//        List<Node> list = new ArrayList<>();
-//        collectNodes(tree.getRoot(), list);
-//        return list;
-//    }
-//
-//    private void collectNodes(Node node, List<Node> list) {
-//        list.add(node);
-//        for (int i = 0; i < node.getChildCount(); i++) {
-//            collectNodes(node.getChild(i), list);
-//        }
-//    }
-//}
+package treecmp.heuristics.tbr;
+
+import pal.tree.Node;
+import pal.tree.Tree;
+import treecmp.heuristics.moves.NniMove;
+import treecmp.heuristics.moves.VirtualNniMove;
+import treecmp.metrics.IncrementalMetric;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Zoptymalizowany Walker dla otoczenia TBR.
+ * Obsługuje w pełni węzły o stopniu > 2 (np. korzeń w drzewach nieukorzenionych uTBR).
+ */
+public class TbrNeighborhoodWalker {
+
+    public interface TbrVisitor {
+        void visit(double distance, Node pruneNode, Node rerootNode, Node targetNode);
+    }
+
+    private TbrUtils tbrUtils = new TbrUtils();
+
+    public void walk(Tree baseTree, IncrementalMetric metric, TbrVisitor visitor) {
+        List<Node> allNodes = getAllNodes(baseTree);
+
+        for (Node pruneNode : allNodes) {
+            if (pruneNode.isRoot() || pruneNode.getParent() == null) continue;
+
+            // Dla korzenia o stopniu 3 (nieukorzenione), po odcięciu jednej gałęzi
+            // pozostają DWA rodzeństwa. Musimy wejść w obie ścieżki!
+            List<Node> startGateways = getSiblings(pruneNode);
+            for (Node startGateway : startGateways) {
+                traverseReroot(pruneNode, pruneNode, startGateway, pruneNode.getParent(), metric, visitor);
+            }
+        }
+    }
+
+    private void traverseReroot(Node pruneNode, Node currentReroot, Node mainTreeGateway, Node cameFrom,
+                                IncrementalMetric metric, TbrVisitor visitor) {
+
+        traverseRegraft(pruneNode, currentReroot, mainTreeGateway, currentReroot, metric, visitor);
+
+        if (!currentReroot.isLeaf()) {
+            for (int i = 0; i < currentReroot.getChildCount(); i++) {
+                Node child = currentReroot.getChild(i);
+                if (child == cameFrom || child == mainTreeGateway) continue;
+
+                // Pobieramy wszystkie pozostałe gałęzie (obsługa stopnia > 2)
+                List<Node> otherChildren = getOtherChildren(currentReroot, child, cameFrom, mainTreeGateway);
+
+                for (Node otherChild : otherChildren) {
+                    NniMove rerootMove = new VirtualNniMove(otherChild, mainTreeGateway, currentReroot);
+
+                    metric.applyNni(rerootMove);
+                    traverseReroot(pruneNode, child, mainTreeGateway, currentReroot, metric, visitor);
+                    metric.undoNni(rerootMove);
+                }
+            }
+        }
+    }
+
+    private boolean traverseRegraft(Node pruneNode, Node rerootNode, Node currentTarget, Node cameFrom,
+                                    IncrementalMetric metric, TbrVisitor visitor) {
+
+        // 1. RUCH W GÓRĘ GŁÓWNEGO DRZEWA (UP)
+        Node p = currentTarget.getParent();
+        if (p != null && p != cameFrom && p != pruneNode) {
+            // Węzeł 'p' może mieć wiele rodzeństwa (np. przy korzeniu)
+            List<Node> aunts = getSiblings(p);
+            for (Node aunt : aunts) {
+                if (aunt != pruneNode) {
+                    NniMove upMove = new VirtualNniMove(rerootNode, aunt, p);
+
+                    metric.applyNni(upMove);
+
+                    if (tbrUtils.isValidTbrMove(pruneNode, rerootNode, p)) {
+                        double exactDist = evaluate(metric);
+                        visitor.visit(exactDist, pruneNode, rerootNode, p);
+                        if (exactDist == 0) { metric.undoNni(upMove); return false; }
+                    }
+
+                    if (!traverseRegraft(pruneNode, rerootNode, p, currentTarget, metric, visitor)) {
+                        metric.undoNni(upMove); return false;
+                    }
+                    metric.undoNni(upMove);
+                }
+            }
+        }
+
+        // 2. RUCH W DÓŁ GŁÓWNEGO DRZEWA (DOWN)
+        if (!currentTarget.isLeaf()) {
+            for (int i = 0; i < currentTarget.getChildCount(); i++) {
+                Node child = currentTarget.getChild(i);
+                if (child == cameFrom || child == pruneNode) continue;
+
+                List<Node> otherChildren = getOtherChildren(currentTarget, child, cameFrom, pruneNode);
+                for (Node otherChild : otherChildren) {
+                    NniMove downMove = new VirtualNniMove(otherChild, rerootNode, currentTarget);
+
+                    metric.applyNni(downMove);
+
+                    if (tbrUtils.isValidTbrMove(pruneNode, rerootNode, child)) {
+                        double exactDist = evaluate(metric);
+                        visitor.visit(exactDist, pruneNode, rerootNode, child);
+                        if (exactDist == 0) { metric.undoNni(downMove); return false; }
+                    }
+
+                    if (!traverseRegraft(pruneNode, rerootNode, child, currentTarget, metric, visitor)) {
+                        metric.undoNni(downMove); return false;
+                    }
+                    metric.undoNni(downMove);
+                }
+            }
+        }
+        return true;
+    }
+
+    // Bezpośrednie wywołanie dystansu (metryka inkrementalna zawsze zna swój stan po wykonanych ruchach NNI)
+    private double evaluate(IncrementalMetric metric) {
+        return metric.getCurrentDistance();
+    }
+
+    // Zwraca listę wszystkich rodzeństw danego węzła
+    private List<Node> getSiblings(Node node) {
+        List<Node> siblings = new ArrayList<>();
+        Node parent = node.getParent();
+        if (parent != null) {
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                if (parent.getChild(i) != node) {
+                    siblings.add(parent.getChild(i));
+                }
+            }
+        }
+        return siblings;
+    }
+
+    // Bezpiecznie filtruje dzieci, omijając te zablokowane
+    private List<Node> getOtherChildren(Node parent, Node exclude1, Node exclude2, Node exclude3) {
+        List<Node> others = new ArrayList<>();
+        for (int i = 0; i < parent.getChildCount(); i++) {
+            Node child = parent.getChild(i);
+            if (child != exclude1 && child != exclude2 && child != exclude3) {
+                others.add(child);
+            }
+        }
+        return others;
+    }
+
+    private List<Node> getAllNodes(Tree tree) {
+        List<Node> list = new ArrayList<>();
+        collectNodes(tree.getRoot(), list);
+        return list;
+    }
+
+    private void collectNodes(Node node, List<Node> list) {
+        list.add(node);
+        for (int i = 0; i < node.getChildCount(); i++) {
+            collectNodes(node.getChild(i), list);
+        }
+    }
+}
